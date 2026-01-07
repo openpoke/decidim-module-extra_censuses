b2849f5a659f1dbca9769071c384afafca995add
Anna Topalidi <t.opalidisanna@gmail.com>
2026-01-07 12:45:40 +0100

    delete tests

--- lib/decidim/extra_censuses/version.rb
+++ lib/decidim/extra_censuses/version.rb
@@ -5,5 +5,9 @@ module Decidim
     VERSION = "0.1.0"
     DECIDIM_VERSION = "0.31.0"
     COMPAT_DECIDIM_VERSION = [">= 0.31.0", "< 0.32"].freeze
+
+    def self.version
+      VERSION
+    end
   end
 end
